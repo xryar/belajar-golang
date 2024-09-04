fee0be309fd114678f9bd1f67bedff8550b8d2c1
Arya Rizki Andaru <aryarizki3045@gmail.com>
2024-09-04 09:59:17 +0700

    Initial Commit

---
+++ belajar-mutex/go.mod
@@ -0,0 +1,3 @@
+module belajar-mutex
+
+go 1.22.3
---
+++ belajar-mutex/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+type counter struct {
+	sync.Mutex
+	val int
+}
+
+func (c *counter) Add(int) {
+	c.Lock()
+	c.val++
+	c.Unlock()
+}
+
+func (c *counter) Value() int {
+	return c.val
+}
+
+func main() {
+	runtime.GOMAXPROCS(2)
+
+	var wg sync.WaitGroup
+	var meter counter
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+
+		go func() {
+			for j := 0; j < 1000; j++ {
+				meter.Add(1)
+			}
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+	fmt.Println(meter.Value())
+}

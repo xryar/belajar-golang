8eda59aa01be292cc6c4b8b7dbe858433565931a
Arya Rizki Andaru <aryarizki3045@gmail.com>
2024-09-24 13:23:01 +0700

    Initial Commit

---
+++ belajar-partial-template/go.mod
@@ -0,0 +1,3 @@
+module belajar-partial-template
+
+go 1.22.3
---
+++ belajar-partial-template/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type M map[string]interface{}
+
+func main() {
+	// buat ngerender semua file html yang ada dalam satu folder
+	// var tmpl, err = template.ParseGlob("views/*")
+	// if err != nil {
+	// 	panic(err.Error())
+	// 	return
+	// }
+
+	http.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
+		var data = M{"name": "Batman"}
+		// ngerender file html yang di pilih aja
+		var tmpl = template.Must(template.ParseFiles(
+			"views/index.html",
+			"views/_header.html",
+			"views/_message.html",
+		))
+
+		var err = tmpl.ExecuteTemplate(w, "index", data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+		var data = M{"name": "Batman"}
+		// ngerender file html yang di pilih aja
+		var tmpl = template.Must(template.ParseFiles(
+			"views/about.html",
+			"views/_header.html",
+			"views/_message.html",
+		))
+
+		var err = tmpl.ExecuteTemplate(w, "about", data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	fmt.Println("server started at localhost:9000")
+	http.ListenAndServe(":9000", nil)
+
+}

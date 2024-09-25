6bff1641dcb70babad0cbb58fa1c8c98a09f4adf
Arya Rizki Andaru <aryarizki3045@gmail.com>
2024-09-25 12:13:40 +0700

    Initial Commit

---
+++ belajar-custom-functions/go.mod
@@ -0,0 +1,3 @@
+module belajar-custom-functions
+
+go 1.22.3
---
+++ belajar-custom-functions/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	f "fmt"
+	"html/template"
+	"net/http"
+)
+
+var funcMap = template.FuncMap{
+	"unescape": func(s string) template.HTML {
+		return template.HTML(s)
+	},
+	"avg": func(n ...int) int {
+		var total = 0
+		for _, each := range n {
+			total += each
+		}
+		return total / len(n)
+	},
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		var tmpl = template.Must(template.New("view.html").
+			Funcs(funcMap).
+			ParseFiles("view.html"))
+		if err := tmpl.Execute(w, nil); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	f.Println("server started at localhost:9000")
+	http.ListenAndServe(":9000", nil)
+}

c8b426238bc437c3d32f359a5c83471e93e46d48
Arya Rizki Andaru <aryarizki3045@gmail.com>
2024-11-05 11:00:23 +0700

    Initial Commit

---
+++ belajar-echo-dan-routing/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/labstack/echo"
+)
+
+type M map[string]interface{}
+
+var ActionIndex = func(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("from action index"))
+}
+
+var ActionHome = http.HandlerFunc(
+	func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("from action home"))
+	},
+)
+
+var ActionAbout = echo.WrapHandler(
+	http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			w.Write([]byte("from action about"))
+		},
+	),
+)
+
+func main() {
+	r := echo.New()
+
+	r.GET("/", func(ctx echo.Context) error {
+		data := "Hello from /index"
+		return ctx.String(http.StatusOK, data)
+	})
+
+	r.GET("/page1", func(ctx echo.Context) error {
+		name := ctx.QueryParam("name")
+		data := fmt.Sprintf("Hello %s", name)
+
+		return ctx.String(http.StatusOK, data)
+	})
+
+	r.GET("/page2/:name", func(ctx echo.Context) error {
+		name := ctx.Param("name")
+		data := fmt.Sprintf("Hello %s", name)
+
+		return ctx.String(http.StatusOK, data)
+	})
+
+	r.GET("/page3/:name/*", func(ctx echo.Context) error {
+		name := ctx.Param("name")
+		message := ctx.Param("*")
+
+		data := fmt.Sprintf("Hello %s, I have message for you: %s", name, message)
+
+		return ctx.String(http.StatusOK, data)
+	})
+
+	r.POST("/page4", func(ctx echo.Context) error {
+		name := ctx.FormValue("name")
+		message := ctx.FormValue("message")
+
+		data := fmt.Sprintf(
+			"Hello %s, I have message for you: %s",
+			name,
+			strings.Replace(message, "/", "", 1),
+		)
+
+		return ctx.String(http.StatusOK, data)
+	})
+
+	r.GET("/index", echo.WrapHandler(http.HandlerFunc(ActionIndex)))
+	r.GET("/home", echo.WrapHandler(ActionHome))
+	r.GET("/about", ActionAbout)
+
+	r.Start(":9000")
+
+}

06248edbe37afccac6b640068490b4608eebc5ac
Arya Rizki Andaru <aryarizki3045@gmail.com>
2024-11-09 14:00:09 +0700

    add error handling

--- belajar-validation-echo/main.go
+++ belajar-validation-echo/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-playground/validator/v10"
@@ -37,6 +38,33 @@ func main() {
 
 		return ctx.JSON(http.StatusOK, true)
 	})
+
+	e.HTTPErrorHandler = func(err error, ctx echo.Context) {
+		report, ok := err.(*echo.HTTPError)
+		if !ok {
+			report = echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+		}
+
+		if castedObject, ok := err.(validator.ValidationErrors); ok {
+			for _, err := range castedObject {
+				switch err.Tag() {
+				case "required":
+					report.Message = fmt.Sprintf("%s is required", err.Field())
+				case "email":
+					report.Message = fmt.Sprintf("%s is not valid email", err.Field())
+				case "gte":
+					report.Message = fmt.Sprintf("%s value must be greater than %s", err.Field(), err.Param())
+				case "lte":
+					report.Message = fmt.Sprintf("%s value must be lower than %s", err.Field(), err.Param())
+				}
+
+				break
+			}
+		}
+
+		ctx.Logger().Error(report)
+		ctx.JSON(report.Code, report)
+	}
 
 	e.Logger.Fatal(e.Start(":9000"))
 }
